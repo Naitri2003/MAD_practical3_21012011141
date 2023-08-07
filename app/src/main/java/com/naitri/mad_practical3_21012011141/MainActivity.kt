b7552b2fb42392d5690aba359aa3bd90bde628b5
Naitri2003 <132800074+Naitri2003@users.noreply.github.com>
2023-08-07 19:50:48 +0530

    Initial commit

---
+++ app/src/main/java/com/naitri/mad_practical3_21012011141/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.naitri.mad_practical3_21012011141
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
